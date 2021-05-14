55edaf7323de5e9f4cb15ce6d19629998c7f1a87
DKstill17 <dkstillo94@gmail.com>
2021-05-14 10:09:34 -0500

    Comentarios

--- src/app.py
+++ src/app.py
@@ -2,14 +2,18 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
+#Realizado por Daniel Castillo & Nefer Blanchar
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root@localhost/flaskdb'
+
+#Configuracion a Mysql
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root@localhost/flaskdb' #Direccion de la BD
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-#Clase Usuarios & Modelo de la BD
+#Definiendo que vamos a guardar en la BD
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(70), unique=True)
@@ -17,7 +21,7 @@ class User(db.Model):
     nombre = db.Column(db.String(70))
     email = db.Column(db.String(70), unique=True)
     admin = db.Column(db.String(1))
-
+  #Definimos un constructor para recibir y asignar datos
     def __init__(self, username, password, nombre, email, admin):
         self.username = username
         self.password = password
@@ -25,14 +29,15 @@ class User(db.Model):
         self.email = email
         self.admin = admin
 
-#Clase Categorias & Modelo de la BD
+#Definiendo que vamos a guardar en la BD
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombreCat = db.Column(db.String(70), unique=True)
-
+#Definimos un constructor para recibir y asignar datos
     def __init__(self, nombreCat):
         self.nombreCat = nombreCat
 
+#Definiendo que vamos a guardar en la BD
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombreArt = db.Column(db.String(70), unique=True)
@@ -52,40 +57,42 @@ class Article(db.Model):
         self.stock = stock 
         self.id_Categories = id_Categories
 
-#Crea todas las tablas
+#Crea todas las tablas de todas las clases
 db.create_all()
 
 #Usuarios
-#Que datos vamos a utilizar
+#Que datos vamos a utilizar creando un esquema
 class UserSchema(ma.Schema):
     class Meta:
         fields = ('id', 'username', 'password', 'nombre', 'email', 'admin')
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
 
-#Asignamos una ruta para luego guardarlos por POST
+user_schema = UserSchema() #Instancia para interactuar con un dato
+users_schema = UserSchema(many=True) #Instancia para interactuar con muchos datos
+
+#Asignamos una ruta para luego guardarlos por POST (endspoints)
 @app.route('/users', methods=['POST'])
 def create_user():
 
    #print(request.json)
    #return 'recibido'
 
-#Decimos que almacene los datos en JSON
+#Recibimos los datos a traves del request
   username = request.json['username']
   password = request.json['password']
   nombre = request.json['nombre']
   email = request.json['email']
   admin = request.json['admin']
 
+#Creamos un esquema de lo que se va a guardar
   new_user = User(username, password, nombre, email, admin)
 
+#La guardamos en la BD
   db.session.add(new_user)
   db.session.commit()
 
-#Se guardan los datos en la BD
+#Al finalizar nos mostrará los datos guardados
   return user_schema.jsonify(new_user)
-
 
 #Se asigna una ruta y luego se hace un llamado GET para traer todos los datos guardados
 @app.route('/users', methods=['GET'])
@@ -96,7 +103,7 @@ def get_users():
 
 #Final Usuarios
 
-#Categorias
+#Categories
 class CategorySchema(ma.Schema):
     class Meta:
         fields = ('id', 'nombreCat')
@@ -107,12 +114,8 @@ categories_schema = CategorySchema(many=True)
 @app.route('/categories', methods=['POST'])
 def create_category():
 
-   #print(request.json)
-   #return 'recibido'
-
   nombreCat = request.json['nombreCat']
  
-
   new_category = Category(nombreCat)
 
   db.session.add(new_category)
@@ -127,9 +130,7 @@ def get_categories():
   return jsonify(result)
 
   #Final Categorias
-
-  #Articulos
-#Que datos vamos a utilizar
+#Articulos
 class ArticleSchema(ma.Schema):
     class Meta:
         fields = ('id', 'nombreArt', 'precio', 'iva', 'descripcion', 'image', 'stock', 'id_Categories')
@@ -137,14 +138,9 @@ class ArticleSchema(ma.Schema):
 article_schema = ArticleSchema()
 articles_schema = ArticleSchema(many=True)
 
-#Asignamos una ruta para luego guardarlos por POST
 @app.route('/articles', methods=['POST'])
 def create_article():
 
-   #print(request.json)
-   #return 'recibido'
-
-#Decimos que almacene los datos en JSON
   nombreArt = request.json['nombreArt']
   precio = request.json['precio']
   iva = request.json['iva']
@@ -158,21 +154,15 @@ def create_article():
   db.session.add(new_article)
   db.session.commit()
 
-#Se guardan los datos en la BD
   return article_schema.jsonify(new_article)
 
-
-#Se asigna una ruta y luego se hace un llamado GET para traer todos los datos guardados
 @app.route('/articles', methods=['GET'])
 def get_articles():
   all_articles = Article.query.all()
   result = articles_schema.dump(all_articles)
   return jsonify(result)
 
-#Final Usuarios
-
-
-
+#Final articulos
 
 if __name__ == "__main__":
     app.run(debug=True)
